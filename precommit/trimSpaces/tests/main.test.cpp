d8b745a072ab62a143f001b262943d69fd85b141
Julio Angulo <solojoas@gmail.com>
2023-06-19 15:38:13 +0000

    Refactoring test using std::function

--- precommit/trimSpaces/tests/main.test.cpp
+++ precommit/trimSpaces/tests/main.test.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
+#include <functional>
+#include <vector>
 #include "stringUtils.test.h"
+
+void printSeparator()
+{
+    std::cout << "---------------------------------------------------------" << std::endl;
+}
 
 int main() {
     std::cout << "***************** Testing trimSpaces tool *********" << std::endl;
-    testStripLeft();
-    std::cout << "---------------------------------------------------------" << std::endl;
-    testStripLeftManyBlankChars();
-    std::cout << "---------------------------------------------------------" << std::endl;
-    testStripRight();
-    std::cout << "---------------------------------------------------------" << std::endl;
+
+    std::vector<std::function<void()>> testFunctions;
+
+    testFunctions.push_back(testStripLeft);
+    testFunctions.push_back(testStripLeftManyBlankChars);
+    testFunctions.push_back(testStripRight);
+
+    for(const auto& test : testFunctions){
+        test();
+        printSeparator();
+    }
 
     return 0;
 }
